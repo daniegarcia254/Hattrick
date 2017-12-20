7cb69ddabafb268f54e89f8d4361bc4546597d14
Daniel Garcoa <daniegarcia254@gmail.com>
2017-12-20 17:55:42 +0100

    Login && logout. Webpack config for different environments

---
+++ client/src/_services/user.service.js
@@ -0,0 +1,101 @@
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch(API_URL + '/users/login', requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject(response.statusText);
+            }
+
+            return response.json();
+        })
+        .then(user => {
+					  console.log("Success login user", user);
+            if (user && user.id) {
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+function logout() {
+		const requestOptions = {
+        method: 'POST',
+        headers: authHeader()
+    };
+    localStorage.removeItem('user');
+		return fetch(API_URL + '/users/logout', requestOptions).then(response=> { return {}; });
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(API_URL + '/users', requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(API_URL + '/users/' + id, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(API_URL + '/users', requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(API_URL + '/users/' + user.id, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(API_URL + '/users/' + id, requestOptions).then(handleResponse);;
+}
+
+function handleResponse(response) {
+    if (!response.ok) {
+        return Promise.reject(response.statusText);
+    }
+
+    return response.json();
+}
