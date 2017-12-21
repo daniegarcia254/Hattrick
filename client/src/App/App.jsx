8474d4e58c56702d77af0f62267a8b3fd4892a0a
dgarciamaubic <dgarcia@maubic.es>
2017-12-21 15:23:19 +0100

    Add header

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
-import { PrivateRoute } from '../_components';
+import { PrivateRoute, Header} from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
@@ -23,19 +23,24 @@ class App extends React.Component {
     render() {
         const { alert } = this.props;
         return (
-            <div className="jumbotron">
-                <div className="container">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        {alert.message &&
-                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
-                        <Router history={history}>
-                            <div>
-                                <PrivateRoute exact path="/" component={HomePage} />
-                                <Route path="/login" component={LoginPage} />
-                                <Route path="/register" component={RegisterPage} />
-                            </div>
-                        </Router>
+            <div>
+                <div>
+                    <Header></Header>
+                </div>
+                <div className="jumbotron">
+                    <div className="container">
+                        <div className="col-sm-8 col-sm-offset-2">
+                            {alert.message &&
+                                <div className={`alert ${alert.type}`}>{alert.message}</div>
+                            }
+                            <Router history={history}>
+                                <div>
+                                    <PrivateRoute exact path="/" component={HomePage} />
+                                    <Route path="/login" component={LoginPage} />
+                                    <Route path="/register" component={RegisterPage} />
+                                </div>
+                            </Router>
+                        </div>
                     </div>
                 </div>
             </div>
