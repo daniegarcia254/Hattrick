7ac1c604a6ed83d82e2e8ad8abc515d7c493233e
dgarciamaubic <dgarcia@maubic.es>
2017-12-21 15:23:05 +0100

    Add webpack loaders for images and sass

--- client/webpack/base.config.js
+++ client/webpack/base.config.js
@@ -1,36 +1,54 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
+
+var extractPlugin = new ExtractTextPlugin({
+	filename: 'main.css'
+});
 
 module.exports = {
 	entry: './src/index.jsx',
 	output: {
-			path: path.resolve('dist'),
-			filename: 'bundle.js'
+		path: path.resolve('dist'),
+		filename: 'bundle.js'
 	},
 	resolve: {
-			extensions: ['.js', '.jsx']
+		extensions: ['.js', '.jsx']
 	},
 	module: {
-			loaders: [
-					{
-							test: /\.jsx?$/,
-							exclude: /(node_modules|bower_components)/,
-							loader: 'babel-loader',
-							query: {
-									presets: ['react', 'es2015', 'stage-3']
-							}
-					}
-			]
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015', 'stage-3']
+				}
+			}, {
+				test: /\.scss$/,
+				use: extractPlugin.extract({
+					use: ['css-loader', 'sass-loader']
+				})
+			}, {
+				test: /\.(png|jp(e*)g|svg)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 8000, // Convert images < 8kb to base64 strings
+					name: 'images/[hash]-[name].[ext]'
+				}
+			}
+		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
 			filename: 'index.html',
 			inject: 'body'
-		})
+		}),
+		extractPlugin
 	],
 	devServer: {
-			historyApiFallback: true
+		historyApiFallback: true
 	}
 }
