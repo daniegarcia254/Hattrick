a3f0e2564299210c975e9272e46e015a865ddac6
daniegarcia254 <daniegarcia254@gmail.com>
2018-12-01 02:04:22 +0100

    Add production config files

---
+++ server/config.production.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var logger = require('../common/logger');
+
+module.exports = {
+	crypto: {
+		algorithm: process.env.CRYPTO_ALG,
+		password: process.env.CRYPTO_PWD
+	},
+	email: {
+		host: process.env.APP_HOST,
+		email: process.env.EMAIL_USER,
+		password: process.env.EMAIL_PWD,
+		service: process.env.EMAIL_SERVICE,
+	},
+	remoting: {
+		errorHandler: {
+			handler: function(err, req, res, next) {
+				// custom error handling logic
+				logger.error("Error", err);
+				next();
+			}
+		}
+	}
+};
